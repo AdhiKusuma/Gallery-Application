c4a7ab229a9c7a9ab5e4088508214131d11720f9
AdhiKusuma <111202012858@mhs.dinus.ac.id>
2022-12-16 19:33:56 +0700

    git pertama

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Adhi Candra Kusuma"; // TODO: replace this
+const studentId = "FE2784289"; // TODO: replace this
+
+export { url, studentName, studentId };
